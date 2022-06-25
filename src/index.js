8020881b93a288e26da402564f19602d159a0616
Luis Badiali <info@badiali.es>
2022-06-25 02:49:42 +0200

    Assets

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
 /* SCSS */
 import './scss/style.scss';
 
-/* Imagenes */
-import './assets/images/favicon.png';
+/* Imágenes */
+import './assets/img/favicon.png';
+import './assets/img/coinbase.svg';
+
+/* Fuentes */
+import './assets/fonts/CoinbaseIcons.woff2';
+import './assets/fonts/CoinbaseSans-Regular.woff2';
+import './assets/fonts/CoinbaseSans-Medium.woff2';
+import './assets/fonts/CoinbaseDisplay-Regular.woff2';
+import './assets/fonts/CoinbaseDisplay-Medium.woff2';
+import './assets/fonts/CoinbaseText-Regular.woff2';
+import './assets/fonts/CoinbaseText-Medium.woff2';
+
+/* Bootstrap components */
+import Dropdown from 'bootstrap/js/dist/dropdown';
+// import Offcanvas from 'bootstrap/js/dist/offcanvas';
+// import Modal from 'bootstrap/js/dist/modal';
+// import Collapse from 'bootstrap/js/dist/collapse';
